58e21be15783a6881a15bb528f668f097f163d03
mytran123 <mytran091196@gmai.com>
2021-10-19 15:35:34 +0700

    bài tập

--- BT_uu-tien-kham-benh/Patient.php
+++ BT_uu-tien-kham-benh/Patient.php
@@ -4,4 +4,20 @@ class Patient
 {
     public $name;
     public $code;
+
+    public function __construct($name, $code)
+    {
+        $this->name = $name;
+        $this->code = $code;
+    }
+
+    public function dequeue()
+    {
+        
+    }
+
+    public function toString()
+    {
+        return $this->name . " " . $this->code;
+    }
 }
---
+++ BT_uu-tien-kham-benh/index.php
@@ -0,0 +1,5 @@
+<?php
+include_once "Patient.php";
+
+$patient = new Patient();
+
