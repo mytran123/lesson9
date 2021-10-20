72abe62acd90fee1de28fd4006699ae87357ec32
mytran123 <mytran091196@gmai.com>
2021-10-20 14:56:06 +0700

    bài tập

--- BT_dao-nguoc-phan-tu/Stack.php
+++ BT_dao-nguoc-phan-tu/Stack.php
@@ -3,19 +3,46 @@
 class Stack
 {
     public $stack;
+    public $limit;
 
-    public function __construct($stack)
+    public function __construct($limit = 5)
     {
         $this->stack = [];
+        $this->limit = $limit;
+    }
+
+    public function isEmpty()
+    {
+        return count($this->stack) <= 0;
+    }
+
+    public function isFull()
+    {
+        return count($this->stack) >= $this->limit;
     }
 
     public function push($item)
     {
-
+        if (!$this->isFull()) {
+            array_unshift($this->stack,$item);
+        } else {
+            echo "Stack is full";
+        }
     }
 
     public function pop()
     {
+        if (!$this->isEmpty()) {
+            array_shift($this->stack);
+        } else {
+            echo "Stack is empty";
+        }
+    }
 
+    public function disPLay()
+    {
+        for ($i = 0; $i < count($this->stack);$i++) {
+            echo $this->stack[$i] . " ";
+        }
     }
 }
---
+++ BT_dao-nguoc-phan-tu/index.php
@@ -0,0 +1,10 @@
+<?php
+include_once "Stack.php";
+
+$stack = new Stack();
+$stack->push(2);
+$stack->push(4);
+$stack->push(6);
+$stack->push(8);
+$stack->pop();
+$stack->disPLay();
